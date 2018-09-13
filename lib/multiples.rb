f301409aa4aecd41ad6c7f696a783663dbb9a432
Flatiron School <andrewsprague@outlook.com>
2018-09-13 09:36:55 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples (target)
+    multiples = (1..target-1).select do |i|
+        i % 3 == 0 || i % 5 == 0
+    end
+
+    
+end
+
+def sum_multiples(target)
+    collect_multiples(target).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+require 'pry'
+
+class Multiples
+
+    attr_reader :top
+
+    def initialize (n)
+        @top = n
+    end
+
+    def collect_multiples
+        (1...top).select do |i|
+            i % 3 == 0 || i % 5 == 0
+        end
+    end
+
+    def sum_multiples
+        self.collect_multiples.reduce(:+)
+    end
+end
+
+# hundred = Multiple.new(100)
+# thousand = Multiple.new(1000)
+# five_hundred = Multiple.new(500)
+
+
+# binding.pry
